0211814821b6847b68a193f3c387837fbf2d49e5
kamkara <lnclass.kamkara@gmail.com>
2022-10-31 10:16:06 +0000

    Add course

---
+++ app/models/course.rb
@@ -0,0 +1,17 @@
+class Course < ApplicationRecord
+  ######### RELATIONS  ########
+  belongs_to :user
+  has_rich_text :content
+
+  ######### VALIDATIONS  ########
+  validates :title, :content, :slug, :user_id, presence: true
+  
+  
+  ######### URL'S  ########
+  extend FriendlyId
+    friendly_id :title, use: :slugged
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
+end
